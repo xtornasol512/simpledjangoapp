0240965e10d0f7c043df7903fae25c7751763004
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-10-05 19:02:49 -0500

    add status to render function

--- chuko/chuko/views.py
+++ chuko/chuko/views.py
@@ -1,25 +1,16 @@
 ''' Custom error pages '''
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 
 def bad_request(request):
-    response = render_to_response('400.html', {}, context_instance=RequestContext(request))
-    response.status_code = 400
-    return response
+    return render(request, "400.html", status=400)
 
 def permission_denied(request):
-    response = render_to_response('403.html', {}, context_instance=RequestContext(request))
-    response.status_code = 403
-    return response
+    return render(request, "403.html", status=403)
 
 
 def page_not_found(request):
-    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+    return render(request, "404.html", status=404)
 
 def server_error(request):
-    response = render_to_response('500.html', {}, context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
+    return render(request, "500.html", status=500)
